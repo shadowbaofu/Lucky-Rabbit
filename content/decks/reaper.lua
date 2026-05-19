b6422b8dcc907eb1e1369a2cad105ba00e90571c
shadowbaofu <134404402+shadowbaofu@users.noreply.github.com>
2026-05-19 17:41:52 -0600

    Adjust blind chips multiplier from 2 to 1.5

--- content/decks/reaper.lua
+++ content/decks/reaper.lua
@@ -5,6 +5,7 @@ SMODS.Back {
     unlocked = true,
     discovered = false,
     config = {
+        dollars = 10,
         hands = 1,
         discards = 1,
         hand_size = 1,
@@ -12,7 +13,7 @@ SMODS.Back {
     },
     calculate = function(self, back, context)
         if context.setting_blind and G.GAME.blind.boss then
-            G.GAME.blind.chips = G.GAME.blind.chips * 2
+            G.GAME.blind.chips = G.GAME.blind.chips * 1.5
             G.GAME.blind.chip_text = number_format(G.GAME.blind.chips)
         end
     end
