89eac7bf0865a260549515dd8faa353758169814
shadowbaofu <134404402+shadowbaofu@users.noreply.github.com>
2026-05-19 17:40:51 -0600

    Update harmony.lua configuration values

--- content/decks/harmony.lua
+++ content/decks/harmony.lua
@@ -4,8 +4,8 @@ SMODS.Back {
     pos = { x = 2, y = 1 },
     unlocked = true,
     discovered = false,
-    config = {
-        discards = -2
+    config = { dollars = 10,
+        discards = -1
     },
     apply = function(self, back)
         G.GAME.starting_params.fmod_harmony = true
